f3408ced082ff32cb6d2d83d9a6053429af8c9d0
nathanpotolsky <nathanpotolsky@gmail.com>
2024-09-17 16:07:29 -0400

    Update Home.py

--- Home.py
+++ Home.py
@@ -2,3 +2,7 @@ import streamlit as st
 import pandas as pd
 
 st.write("""# Welcome to Cyber Compass""")
+
+st.write("""
+Cyber Compass is an AI chat interface where users can search for roles that align to their expertise. This tool will provide career guidance to transitioning service members or federal civilians who are interested in identifying roles that align to their experience/background.
+""")
